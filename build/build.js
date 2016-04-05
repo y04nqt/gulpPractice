72ac891dc3c00a1dc7bbd445788b04332bc35361
Aaron <akr.git.receiver@gmail.com>
2016-04-05 09:58:30 -0500

    another practice of gulp

---
+++ build/build.js
@@ -0,0 +1,33 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// define an ES6 class called HelloWorld
+
+var HelloWorld = function () {
+  function HelloWorld() {
+    _classCallCheck(this, HelloWorld);
+  }
+
+  _createClass(HelloWorld, [{
+    key: 'toString',
+
+    // define a toString() method on the class
+    value: function toString() {
+      return 'Hello, world!';
+    }
+  }]);
+
+  return HelloWorld;
+}();
+// instantiate a new instance of HelloWorld
+
+
+var sample = new HelloWorld();
+// console.log sample's toString() method
+console.log(sample.toString());
+
+},{}]},{},[1]);
